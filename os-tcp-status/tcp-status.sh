ff8fd2f9a73b4e33804a1144b1defeb85b31e996
Damon.Yang <damonops@163.com>
2020-06-12 12:08:38 +0800

    Update tcp-status.sh

--- os-tcp-status/tcp-status.sh
+++ os-tcp-status/tcp-status.sh
@@ -19,8 +19,8 @@ if [ $# -ne 1 ];then
 fi
 
 tcp_status_fun() {
-
-    [ "$tcp_stat" = "ESTABLISHED" ] && tcp_stat="ESTAB" || tcp_stat=$1
+    tcp_stat=$1
+    [ "$tcp_stat" = "ESTABLISHED" ] && tcp_stat="ESTAB"
 
     ss -ant|awk 'NR>1 {++s[$1]} END {for (k in s) print k,s[k]}' >$tmp_file
     tcp_status=$(grep $tcp_stat $tmp_file|cut -d' ' -f2)
